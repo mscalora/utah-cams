639940170e854a97ca15ca6c3127b397acb6dfa6
Mike Scalora <mike@scalora.org>
2020-02-03 05:19:59 -0700

    Changed prism camera strategy, enabled YouTube links also

--- cam-data-test.js
+++ cam-data-test.js
@@ -2,29 +2,123 @@ window.test_data = {
   "sundace": {
     "title" : "Sundance",
     "link" : "Sundance Cams",
-    "prism" : {
-      metadataUrl: "https://php.prismcam.com/public/helpers/cam_init.php",
-      metaDataParams: {cams:'',pages:1546311408,url: 'https://www.sundanceresort.com/mountain-report/'},
-      camFields: {
-        collectionPath: "pages",
-        namePath: "${cam.camTitle}",
-        urlPath: "https://storage.googleapis.com/prism-cam-${current.imagePath}/720.jpg"
+    "special": {
+      "type": "html",
+      "css": [
+        "//js.prismcam.com/violet.css"
+      ],
+      "js": [
+        "//js.prismcam.com/violet.js"
+      ],
+      "html": `<div class="prism-wrap"><div id='violet-1546311408' cam='74' data-mode='embed' class="violet-embed"></div></div>
+               <div class="prism-wrap"><div id='violet-1546311408' cam='75' data-mode='embed' class="violet-embed"></div></div>`,
+      "refresh": function () {
+        let done = false;
+        function init () {
+          try {
+            if (!window.Violet) {
+              console.warn('Not ready yet');
+              return;
+            }
+            Violet.Core.init();
+            done = true;
+            $('.prism-wrap').each(function (it) {
+              this.addEventListener('click', function (evt) {
+                $('html').toggleClass('prism-alt');
+                evt.stopImmediatePropagation();
+                Violet.Core.init();
+              }, {capture: true});
+            });
+          } catch (e) { }
+        }
+        init();
+        if (!done) {
+          let timer = setInterval(function () {
+            if (!done) {
+              init();
+            } else {
+              done = true;
+              clearInterval(timer);
+            }
+          }, 250);
+        }
       }
     },
-    cams: [ ]
   },
-	"deervalley": {
-		"title" : "Deer Valley",
-		"link" : "Deer Valley Cams",
+  "solitude": {
+    "title" : "Solitude Mountain",
+    "link" : "Solitude Mountain Cams",
+    "cams": [
+      /*["Roundhouse 2", "https://webcams.solitudemountain.com/rh2.jpg"],*/
+      ["Moonbeam Lodge", "https://webcams.solitudemountain.com/mbl.jpg"]
+    ]
+  },
+	"other": {
+		"title" : "Other",
+		"link" : "Other Cameras",
 		"cams": [
-			["Empire","//cdn.deervalley.com/web/Content/images/webcams/empire.jpg"], 
+			["Empire","//udottraffic.utah.gov/1_devices/aux172.jpeg"],
 		]
 	},
-  "other": {
-    "title": "Interesting Regional",
-    "link": "Miscellaneous Interesting Webcams",
-    "cams": [
-      ["Park City", "https://b6.hdrelay.com/camera/e44d449a-8d01-4c47-8394-14bbf77c273e/snapshot"]
-    ]
-  }
+	"ytlive": {
+		"title" : "Live Animals on YouTube",
+		"link" : "LiveAnimals",
+    "special" : {
+      "type": 'html',
+      "html": `
+        <div class="yt-live"><h4>Puppies</h4><iframe width="560" height="315" src="https://www.youtube.com/embed/fdMKbZnx0LI?controls=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <div class="yt-live"><h4>Kittens</h4><iframe width="560" height="315" src="https://www.youtube.com/embed/M5huFQWHyVI?controls=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <div class="yt-live"><h4>Fish</h4><iframe width="560" height="315" src="https://www.youtube.com/embed/HYknTgcw2BM?controls=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <div class="yt-live"><h4>Birds</h4><iframe width="560" height="315" src="https://www.youtube.com/embed/tHKvusCEic4?controls=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+      `
+    },
+    "subtitle_html": "<span class='smaller'>[from around the world]</span><br>BETA TEST EXPERIENCE"
+	}
 };
+
+function loadDataStuff () {
+  (document.head || document.body).insertAdjacentHTML('beforeend', `
+      <style id="data-extra-styles">
+        .prism-wrap {
+            width: 400px !important;
+        }
+        html.prism-alt .prism-wrap,
+        html.is-phone .prism-wrap {
+            width: calc( 100% - 10px) !important;
+        }
+        @media (max-width: 825px) {
+          .prism-wrap {
+              width: calc( 100% - 10px) !important;
+          }        
+        }
+        .prism-wrap {
+            margin: 5px;
+        }
+        .prism-wrap .hover {
+            opacity: 0 !important;
+        }
+    
+        .prism-wrap .shell > * {
+            transform: none !important;
+        }      
+        .prism-wrap .shell .time {
+            margin-bottom: unset !important;
+        }      
+        .prism-wrap .shell .alert-hover .icon {
+            display: none;
+        }
+        .yt-live {
+            margin: 0 5px;
+        }
+        .yt-live h4 {
+            margin: 5px 0;
+            text-align: center;
+        }
+     </style>`);
+}
+
+if (/comp|inter|loaded/.test(document.readyState)) {
+  loadDataStuff();
+} else {
+  document.addEventListener('DOMContentLoaded', loadDataStuff, {once: true, passive: true});
+}
--- cam-data.js
+++ cam-data.js
@@ -18,19 +18,51 @@ window.cam_data = {
 			["LCC - White Pine Parking - MP 9.2", "https://udottraffic.utah.gov/1_devices/aux16270.jpeg"]
 		]
 	},
-  "sundance": {
+  "sundace": {
     "title" : "Sundance",
-      "link" : "Sundance Cams",
-      "prism" : {
-      metadataUrl: "https://php.prismcam.com/public/helpers/cam_init.php",
-        metaDataParams: {cams:'',pages:1546311408,url: 'https://www.sundanceresort.com/mountain-report/'},
-      camFields: {
-        collectionPath: "pages",
-          namePath: "${cam.camTitle}",
-          urlPath: "https://storage.googleapis.com/prism-cam-${current.imagePath}/720.jpg"
+    "link" : "Sundance Cams",
+    "special": {
+      "type": "html",
+      "css": [
+        "//js.prismcam.com/violet.css"
+      ],
+      "js": [
+        "//js.prismcam.com/violet.js"
+      ],
+      "html": `<div class="prism-wrap"><div id='violet-1546311408' cam='74' data-mode='embed' class="violet-embed"></div></div>
+               <div class="prism-wrap"><div id='violet-1546311408' cam='75' data-mode='embed' class="violet-embed"></div></div>`,
+      "refresh": function () {
+        let done = false;
+        function init () {
+          try {
+            if (!window.Violet) {
+              console.warn('Not ready yet');
+              return;
+            }
+            Violet.Core.init();
+            done = true;
+            $('.prism-wrap').each(function (it) {
+              this.addEventListener('click', function (evt) {
+                $('html').toggleClass('prism-alt');
+                evt.stopImmediatePropagation();
+                Violet.Core.init();
+              }, {capture: true});
+            });
+          } catch (e) { }
+        }
+        init();
+        if (!done) {
+          let timer = setInterval(function () {
+            if (!done) {
+              init();
+            } else {
+              done = true;
+              clearInterval(timer);
+            }
+          }, 250);
+        }
       }
     },
-    cams: [ ]
   },
 	"provo": {
 		"title" : "Provo Canyon",
@@ -115,14 +147,15 @@ window.cam_data = {
 			["Island In The Sky","http://eldesierto.org/Isky.jpg"],
 			["Mesa Verde","http://eldesierto.org/Mverde.jpg"],
 			["Bald Mesa","http://eldesierto.org/BaldMesa.jpg"],
-			["Park City","https://b6.hdrelay.com/camera/e44d449a-8d01-4c47-8394-14bbf77c273e/snapshot"],
+			// ["Park City","https://b6.hdrelay.com/camera/e44d449a-8d01-4c47-8394-14bbf77c273e/snapshot"],
 			["U of U Weather", "http://timecam.tv/upload_image_tc/metwest/MeteoWest.jpg"],
 			["BYU Weather","https://marvin.byu.edu/Weather/webcam800.jpg"],
 			["Zions NP","https://www.nps.gov/webcams-zion/camera.jpg"],
 			["Bear Lake","http://bearlakeweather.com/upload/netcam.jpg"],
-			["Bountiful Bench SW","https://www.rogowx.org/SW_1min_000.jpg"],
-			["Bountiful Bench S","https://www.rogowx.org/S_1min_000.jpg"],
-			["Utah Lake","https://nexusapi.dropcam.com/get_image?width=1900&uuid=0731a258218a462f9c822c967170d36d"],
+			// ["Bountiful Bench SW","https://www.rogowx.org/SW_1min_000.jpg"],
+			// ["Bountiful Bench S","https://www.rogowx.org/S_1min_000.jpg"],
+			// ["Utah Lake","https://nexusapi.dropcam.com/get_image?width=1900&uuid=0731a258218a462f9c822c967170d36d"],
+			["Utah Lake","https://nexusapi-us1.camera.home.nest.com/get_image?uuid=0731a258218a462f9c822c967170d36d&width=1080&public=cDr5GtmWpl"],
 			["Alpine","https://www.wasatchhiker.com/wp-content/uploads/weather/weathercam.jpg"],
 			["Oak City NE","https://www.finarv.net/weather/cam_1.jpg"],
 			["Oak City S","https://www.finarv.net/weather/cam_2.jpg"],
@@ -148,5 +181,59 @@ window.cam_data = {
 		"inactive-cams" : [
 			["US40 @ Daniels Summit MP 33.45 -", "https://udottraffic.utah.gov/1_devices/aux15578.jpeg", "daniels-summit-mp-33"]
 		]
+  },
+  "ytlive": {
+    "title" : "Live Animals on YouTube",
+    "link" : "LiveAnimals",
+    "special" : {
+      "type": 'html',
+      "html": `
+        <div class="yt-live"><h4>Puppies</h4><iframe width="560" height="315" src="https://www.youtube.com/embed/fdMKbZnx0LI?controls=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <div class="yt-live"><h4>Kittens</h4><iframe width="560" height="315" src="https://www.youtube.com/embed/M5huFQWHyVI?controls=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <div class="yt-live"><h4>Fish</h4><iframe width="560" height="315" src="https://www.youtube.com/embed/HYknTgcw2BM?controls=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+        <div class="yt-live"><h4>Birds</h4><iframe width="560" height="315" src="https://www.youtube.com/embed/tHKvusCEic4?controls=0&autoplay=1" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+      `
+    },
+    "subtitle_html": "<span class='smaller'>[from around the world]</span><br>BETA TEST EXPERIENCE"
 	}
 };
+
+function loadDataStuff () {
+  (document.head || document.body).insertAdjacentHTML('beforeend', `
+      <style id="data-extra-styles">
+        .prism-wrap {
+            width: 400px !important;
+        }
+        html.prism-alt .prism-wrap,
+        html.is-phone .prism-wrap {
+            width: calc( 100% - 10px) !important;
+        }
+        @media (max-width: 825px) {
+          .prism-wrap {
+              width: calc( 100% - 10px) !important;
+          }        
+        }
+        .prism-wrap {
+            margin: 5px;
+        }
+        .prism-wrap .hover {
+            opacity: 0 !important;
+        }
+    
+        .prism-wrap .shell > * {
+            transform: none !important;
+        }      
+        .prism-wrap .shell .time {
+            margin-bottom: unset !important;
+        }      
+        .prism-wrap .shell .alert-hover .icon {
+            display: none;
+        }
+     </style>`);
+}
+
+if (/comp|inter|loaded/.test(document.readyState)) {
+  loadDataStuff();
+} else {
+  document.addEventListener('DOMContentLoaded', loadDataStuff, {once: true, passive: true});
+}
